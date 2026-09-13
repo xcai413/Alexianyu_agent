f99c535f9141c0058ae8d6d6a46f88d6f9c988b0
xcai413 <mrlll17191012@gmail.com>
2026-09-14 02:09:13 +0800

    feat(message): add outbound send application package

---
+++ src/xianyu_agent/application/message/__init__.py
@@ -0,0 +1,13 @@
+"""Message application services."""
+
+from xianyu_agent.application.message.send_service import (
+    SendAttemptStatus,
+    SendMessageResult,
+    SendMessageService,
+)
+
+__all__ = [
+    "SendAttemptStatus",
+    "SendMessageResult",
+    "SendMessageService",
+]
